3388e9e0863bd0da89e69e990395ef2bd49afbff
jonny <jonny@immerda.ch>
2022-06-14 14:32:10 +0200

    cleanup files

---
+++ mgmt-k8s/secrets.tf
@@ -0,0 +1,24 @@
+###################
+#   define vars   #
+###################
+variable "vault_login_username" {
+  default   = ""
+  sensitive = true
+}
+
+variable "vault_login_password" {
+  default   = ""
+  sensitive = true
+}
+
+variable "ssh_public_key" {
+  default   = ""
+  sensitive = true
+}
+
+###################
+#  fetch secrets  #
+###################
+data "vault_generic_secret" "hetzner_tokens" {
+  path = "ounu/hetzner/tokens"
+}
